8ead42b8fadc96ca83ecb04211c67727f8d0743b
Sandy-2015 <74339524+Sandy-2015@users.noreply.github.com>
2023-03-10 20:16:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var num =10
+
+
+var student = {
+  name : "Edgar",
+  roll_no : 12,
+  grade : 8,
+  subject : "science"
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+
+  console.log(student.name)
+  console.log(student.subject)
+}
+
+function draw() {
+  background(220);
+
+}
